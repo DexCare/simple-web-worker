5eae4edc03d605a7ff13e4ef429f3dbb93a4dcca
Thomas Hallock <thomas.hallock@providence.org>
2018-11-14 22:45:39 -0800

    forked to remove unnecessary module-scope messaging

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import { create } from './create'
 import { run } from './run'
 
 const createWrapper = () => {
+  if ('undefined' === typeof window) {
+    return null;
+  }
   if (!window.Worker) {
-    console.error('This browser does not support Workers.')
     return null
   }
   if (!(window.URL.createObjectURL || window.webkitURL.createObjectURL)) {
-    console.error('This browser does not have URL.createObjectURL method.')
     return null
   }
   return { create, run }
