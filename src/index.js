af282c1b5de3d99da95ab8320070574fa633998f
Thomas Hallock <thomas.hallock@providence.org>
2018-11-14 22:35:23 -0800

    removed import messaging from module scope altogether because it does not belong here

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 import { create } from './create'
 import { run } from './run'
 
-const createWrapper = () => {
-  if ('undefined' === typeof window) {
-    return null;
-  }
-  if (!window.Worker) {
-    console.error('This browser does not support Workers.')
-    return null
-  }
-  if (!(window.URL.createObjectURL || window.webkitURL.createObjectURL)) {
-    console.error('This browser does not have URL.createObjectURL method.')
-    return null
-  }
-  return { create, run }
-}
-
-const WorkerWrapper = createWrapper()
-
-export default WorkerWrapper
+export default { create, run }
